d2e8846de2679e5d47e0ab479518ee75a52cbcda
Neil Blue <neil.the.blue+gh1@gmail.com>
2016-06-08 23:54:46 +0100

    Create prep_ssmtp.sh
    Creation

---
+++ prep_ssmtp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Setup ssmtp"
+
+CONF=/etc/ssmtp/ssmtp.conf
+rm $CONF
+
+for E in $(env)
+do
+  if [ "$(echo $E | sed -e '/^SSMTP_/!d' )" ]
+  then
+    echo $E | sed -e 's/^SSMTP_//' >> $CONF
+  fi
+done
+
+exec "$@"
